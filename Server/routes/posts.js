0006a3eb8ed15305f0546c7140eeccdaa13bf771
edxu <1614257233@qq.com>
2022-02-20 17:12:04 -0500

    Backend Folder Structure

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
+import { getPosts, createPost } from '../controllers/posts.js';
+
 const router = express.Router();
 
 // going to localhost:5000/posts
 // http://localhost:5000/posts
-router.get('/', (req, res) => {
-    res.send('This works!');
-})
+router.get('/', getPosts);
+
+router.post('/', createPost);
 
 export default router;
 
