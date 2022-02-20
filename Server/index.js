8dbb2cf97f9ca3e430c8d04acd582b7f2292f31b
edxu <1614257233@qq.com>
2022-02-20 16:52:10 -0500

    Connection to MongoDB

--- Server/index.js
+++ Server/index.js
@@ -9,3 +9,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://edxu888:huawei.123@cluster0.wjwzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port:   ${PORT}`)))
+    .catch( (error) => console.log(error.message) );
+    
+// mongoose.set('useFindAndModify', false);
+
+
+
